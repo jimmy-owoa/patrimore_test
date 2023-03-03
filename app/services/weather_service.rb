c2972cef2a8a7568a597219c7ffa47dd1c677b75
Jeremias Gutierrez <jeremiasgup@gmail.com>
2023-03-02 23:25:38 -0300

    feat - weather service cached

--- app/services/weather_service.rb
+++ app/services/weather_service.rb
@@ -1,11 +1,28 @@
-require "httparty"
+require 'redis'
+require 'httparty'
+
 class WeatherService
   def initialize
     @api_key = Rails.application.credentials.weatherapi.api_key
+    @cache = Redis.current
   end
   
   def get_weather_data(q)
-    response = HTTParty.get("https://api.weatherapi.com/v1/current.json?key=#{@api_key}&q=#{q}&aqi=no")
-    response.parsed_response
+    # Try to get the data from the cache
+    cache_key = "weather:#{q}"
+    cached_data = @cache.get(cache_key)
+
+    if cached_data
+      # If the data is in the cache, return it
+      JSON.parse(cached_data)
+    else
+      # If the data is not in the cache, get it from the API
+      response = HTTParty.get("https://api.weatherapi.com/v1/current.json?key=#{@api_key}&q=#{q}&aqi=no")
+      parsed_response = response.parsed_response
+
+      # Store the data in the cache
+      @cache.set(cache_key, parsed_response.to_json)
+      parsed_response
+    end
   end
 end
